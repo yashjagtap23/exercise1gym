444634e43d84effc14b85ac07c67997f90bb05f0
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2024-12-31 20:38:20 -0600

    Initial Commit

---
+++ exercise1gym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise1gym
+//
+//  Created by Yash Jagtap on 12/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ exercise1gymTests/exercise1gymTests.swift
@@ -0,0 +1,17 @@
+//
+//  exercise1gymTests.swift
+//  exercise1gymTests
+//
+//  Created by Yash Jagtap on 12/31/24.
+//
+
+import Testing
+@testable import exercise1gym
+
+struct exercise1gymTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
